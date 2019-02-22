698a501f12ec860065a6aa9d2dac15893e7592a7
tatsuro-nishimura <ntatsuro127@gmail.com>
2019-02-22 15:30:47 +0900

    change

--- nlp_quiz.py
+++ nlp_quiz.py
@@ -20,12 +20,17 @@ def a04(str0):
     list1 = [list0[i][0] if i in [0,4,5,6,7,8,14,15,18] else list0[i][0:2] for i in range(0,len(list0))]
     return dict([(list1[i],i+1) for i in range(0,len(list1))])
 
+def a05(seq0, n):
+    return [seq0[i:i+n] for i in range(0,len(seq0)-n+1)]
+
 def main():
     print(a00('stressed'))
     print(a01('パタトクカシーー'))
     print(a02('パトカー', 'タクシー'))
     print(a03('Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'))
     print(a04('Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.'))
+    print(a05('I am an NLPer'.split(), 2))
+    print(a05('I am an NLPer', 2))
 
 if __name__ == '__main__':
     main()
