b3b5565e6d2edda36b83ced43322ac0602d6b1e6
tatsuro-nishimura <ntatsuro127@gmail.com>
2019-02-22 15:04:32 +0900

    change

--- nlp_quiz.py
+++ nlp_quiz.py
@@ -1,5 +1,5 @@
 # try to answer the problems in http://www.cl.ecei.tohoku.ac.jp/nlp100/
-# not completed
+# sorry, not completed
 import re
 
 
@@ -15,11 +15,17 @@ def a02(str0, str1):
 def a03(str0):
     return [len(x) for x in re.sub('[,.]', '', str0).split(' ')]
 
+def a04(str0):
+    list0 = str0.replace('.','').split()
+    list1 = [list0[i][0] if i in [0,4,5,6,7,8,14,15,18] else list0[i][0:2] for i in range(0,len(list0))]
+    return dict([(list1[i],i+1) for i in range(0,len(list1))])
+
 def main():
     print(a00('stressed'))
     print(a01('パタトクカシーー'))
     print(a02('パトカー', 'タクシー'))
     print(a03('Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'))
+    print(a04('Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.'))
 
 if __name__ == '__main__':
     main()
