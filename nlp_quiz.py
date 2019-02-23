6537c006760c2dec2614a271fbcb89e17539bdd0
tatsuro-nishimura <ntatsuro127@gmail.com>
2019-02-23 17:27:41 +0900

    change

--- nlp_quiz.py
+++ nlp_quiz.py
@@ -1,8 +1,9 @@
 # try to answer the problems in http://www.cl.ecei.tohoku.ac.jp/nlp100/
-# sorry, not completed
+# sorry, under construction
 import re
 import random
 import pandas as pd
+import numpy as np
 
 
 def a00(str):
@@ -41,17 +42,28 @@ def shuffle_str(str):
 def a09(sentence):
     return ' '.join([word[0]+shuffle_str(word[1:-1])+word[-1] if len(word)>4 else word for word in sentence.split()])
 
-def a10_13(input):
+def a10_13(input, N):
     f = open(input)
-    doc = f.read()
-    num_row = doc.count('\n')
+    num_row = 0
+    for i in f:
+        print(i)
+        num_row += 1
     print(num_row)
-    print(doc.replace('\t', ' '))
-    df = pd.DataFrame([x.split(' ') for x in doc.replace('\t', ' ').split('\n') if len(x)>0])
-    cols = [col1, col2] = [df.iloc[:,0], df.iloc[:,1]]
+    print('---' + '\n' + 'complete Q10! (except Linux com)' + '\n' + '---')
+    f = open(input)
+    list0 = []
+    for i in f:
+        i = i.replace('\t', ' ')
+        print(i)
+        list0 += [i.replace('\n','').split(' ')]
+    print('---' + '\n' + 'complete Q11! (except Linux com)' + '\n' + '---')
+    df = pd.DataFrame(list0).rename(
+        columns={0:'prefecture', 1:'city', 2:'high_temprature', 3:'date'})
+    cols = [col1, col2] = [df['prefecture'], df['city']]
     for i in range(1, 3):
         f = open('./col'+str(i)+'.txt', mode='w')
         f.write('\n'.join([x for x in cols[i-1]]))
+    print('---' + '\n' + 'complete Q12! (except Linux com)' + '\n' + '---')
     f = open('./col1.txt'), open('./col2.txt')
     list = []
     for i in range(num_row):
@@ -59,6 +71,22 @@ def a10_13(input):
     f = open('./cols.txt', mode='w')
     f.write(''.join(list))
     f.close()
+    print('---' + '\n' + 'complete Q13! (except Linux com)' + '\n' + '---')
+    print(df.head(N).to_string(index=False, header=False))
+    print(df.tail(N))
+    print(np.array_split(list0, N))
+    print(set(col1))
+    df = df.sort_values(
+        by=['high_temprature'], ascending=False).reset_index(drop=True)
+    print(df)
+    df_col1_size = pd.DataFrame(
+        df.groupby('prefecture').size()).reset_index(
+            ).rename(columns={0:'pref_count'})
+    print(df_col1_size)
+    df1 = pd.merge(df, df_col1_size, on='prefecture', how='left')
+    df1 = df1.sort_values(
+        by=['pref_count'], ascending=False).reset_index(drop=True)
+    print(df1.loc[:,df1.columns!='pref_count'])
 
 def main():
     print(a00('stressed'))
@@ -74,7 +102,8 @@ def main():
     print(cipher(cipher('I am an NLPer')))
     print(a09('Carl Friedrich Gauss was a German mathematician and physicist who made significant contributions to many fields in mathematics and sciences'))
     print('---' + '\n' + 'complete chapter one!' + '\n' + '---')
-    a10_13('./hightemp.txt')
+    a10_13('./hightemp.txt', 5)
+    print('---' + '\n' + 'complete chapter two!(except linux com)' + '\n' + '---')
 
 if __name__ == '__main__':
     main()
