7b5fcca9d07cb48eb2d36570d3584b7e799875dc
tatsuro-nishimura <ntatsuro127@gmail.com>
2019-02-24 00:49:05 +0900

    change

--- test_janome_tokenizer.py
+++ test_janome_tokenizer.py
@@ -1,11 +1,14 @@
 from janome.tokenizer import Tokenizer
 from collections import Counter
 
-def main():
+def count_noun(sentence):
     t = Tokenizer()
-    s = 'なんというベストタイミング魔法使いによって頭を爬虫類の姿に変えられたカイマンが、本来の姿を取り戻すため、友達のニカイドウと共に、魔法使い狩りに立ち上がる！'
-    list_noun = [token.surface for token in t.tokenize(s) if token.part_of_speech.startswith('名詞')]
-    print(Counter(list_noun).most_common())
+    list_noun = [token.surface for token in t.tokenize(sentence) if token.part_of_speech.startswith('名詞')]
+    return Counter(list_noun).most_common()
+
+def main():
+    c = count_noun('なんというベストタイミング魔法使いによって頭を爬虫類の姿に変えられたカイマンが、本来の姿を取り戻すため、友達のニカイドウと共に、魔法使い狩りに立ち上がる！')
+    print(c)
 
 
 if __name__ == '__main__':
