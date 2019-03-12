b419e2c0c5b4f6b39cce0aa3d6d6acba80d9fd27
tatsuro-nishimura <ntatsuro127@gmail.com>
2019-03-12 17:04:59 +0900

    change

--- test_pca.r
+++ test_pca.r
@@ -1,6 +1,6 @@
 result <- prcomp(iris[,-5], scale = T)
 #'scale=T': using correlation matrix
-#'scale=F': using covariance matrix
+#default: using covariance matrix
 summary(result)
 data_PC1_2 <- result$x[,1:2]
 plot(data_PC1_2, col = iris[,5], pch = 16)
