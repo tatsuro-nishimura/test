081decd768979ee900f34cea299b4a7e951fe784
tatsuro-nishimura <ntatsuro127@gmail.com>
2019-03-12 16:48:32 +0900

    change

--- test_pca.r
+++ test_pca.r
@@ -1,3 +1,4 @@
 result <- prcomp(iris[,-5], scale = T)
-pca_data <- result$x[,1:2]
-plot(pca_data, col = iris[,5], pch = 16)
+summary(result)
+data_PC1_2 <- result$x[,1:2]
+plot(data_PC1_2, col = iris[,5], pch = 16)
