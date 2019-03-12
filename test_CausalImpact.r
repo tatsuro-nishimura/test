684e60cdd64c38bd556b7c69d8b409485b86437f
tatsuro-nishimura <ntatsuro127@gmail.com>
2019-03-12 12:07:55 +0900

    change

--- test_CausalImpact.r
+++ test_CausalImpact.r
@@ -1,5 +1,5 @@
 library(CausalImpact)
-x <- arima.sim(n=200, list(arima=0.99))
+x <- arima.sim(n=200, list(arima=.99))
 y <- 1.6*x + rnorm(200)
 y[141:200] <- y[141:200] + 10
 impact <- CausalImpact(cbind(y, x), c(1, 140), c(141, 200))
