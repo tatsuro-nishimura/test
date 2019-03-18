29052943a94d8271f348e49e1ec1bd3dfcd51904
tatsuro-nishimura <ntatsuro127@gmail.com>
2019-03-18 12:48:25 +0900

    change

--- test_lm.r
+++ test_lm.r
@@ -6,7 +6,7 @@ summary(result)
 # multiple linear regression
 lm(iris[,4]~iris[,1]+iris[,2]+iris[,3])
 
-# logistic regression@# y ~ (1+exp(-(a1*x1+a2*x2+....)))^{-1}
+# logistic regression
 glm(c(iris[,5])%%2~iris[,1]+iris[,2]+iris[,3],family=binomial)
 
 library(glmnet)
