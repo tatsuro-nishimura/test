048ce9b9b083b42c90357cd94cc68c3eb1b3906e
tatsuro-nishimura <ntatsuro127@gmail.com>
2019-03-18 12:46:54 +0900

    change

--- test_lm.r
+++ test_lm.r
@@ -8,3 +8,12 @@ lm(iris[,4]~iris[,1]+iris[,2]+iris[,3])
 
 # logistic regression@# y ~ (1+exp(-(a1*x1+a2*x2+....)))^{-1}
 glm(c(iris[,5])%%2~iris[,1]+iris[,2]+iris[,3],family=binomial)
+
+library(glmnet)
+# Lasso regression
+result <- cv.glmnet(as.matrix(iris[,1:3]),as.matrix(iris[,4],ncol=1),alpha = 1)
+coefficients(result,s="lambda.min")
+
+# Ridge regression
+result <- cv.glmnet(as.matrix(iris[,1:3]),as.matrix(iris[,4],ncol=1),alpha = 0)
+coefficients(result,s="lambda.min")
