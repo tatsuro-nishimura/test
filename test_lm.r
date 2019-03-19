1edd9e8f8c3fe826fe571f258221da3ca27cd1e5
tatsuro-nishimura <ntatsuro127@gmail.com>
2019-03-19 19:52:25 +0900

    change

--- test_lm.r
+++ test_lm.r
@@ -1,24 +1,28 @@
 # single linear regression
-result <- lm(iris[,4]~iris[,3])
+result <- lm(iris[,4] ~ iris[,3])
 result$coefficient
 summary(result)
 
 # multiple linear regression
-lm(iris[,4]~iris[,1]+iris[,2]+iris[,3])
+lm(iris[,4] ~ iris[,1] + iris[,2] + iris[,3])
 
 # logistic regression
-glm(c(iris[,5])%%2~iris[,1]+iris[,2]+iris[,3],family=binomial)
+glm(c(iris[,5])%%2 ~ iris[,1] + iris[,2] + iris[,3], family=binomial)
 
 library(glmnet)
 # Lasso regression
-result <- cv.glmnet(as.matrix(iris[,1:3]),as.matrix(iris[,4],ncol=1),alpha = 1)
+result <- cv.glmnet(as.matrix(iris[,1:3]), as.matrix(iris[,4], ncol=1), alpha = 1)
 coefficients(result,s="lambda.min")
 
 # Ridge regression
-result <- cv.glmnet(as.matrix(iris[,1:3]),as.matrix(iris[,4],ncol=1),alpha = 0)
+result <- cv.glmnet(as.matrix(iris[,1:3]), as.matrix(iris[,4], ncol=1), alpha = 0)
+coefficients(result,s="lambda.min")
+
+# elastic net
+result <- cv.glmnet(as.matrix(iris[,1:3]), as.matrix(iris[,4], ncol=1), alpha = .5)
 coefficients(result,s="lambda.min")
 
 # multinomial logistic regression
-result <- cv.glmnet(as.matrix(iris[,-5]),as.matrix(iris[,5],ncol=1), family="multinomial", type.multinomial="grouped")
+result <- cv.glmnet(as.matrix(iris[,-5]), as.matrix(iris[,5],ncol=1), family="multinomial", type.multinomial="grouped")
 pred <- predict(result, newx=as.matrix(iris[,-5]), type="class")
 table(pred, iris[,5])
