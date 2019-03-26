5f078e09740190f4c737b6fc9a6ded62a5c55148
tatsuro-nishimura <ntatsuro127@gmail.com>
2019-03-26 18:31:42 +0900

    change

--- test_part_granger.r
+++ test_part_granger.r
@@ -1,13 +1,14 @@
 library(devtools)
 library(gtools)
 source_url('https://raw.githubusercontent.com/cran/FIAR/master/R/partGranger.R')
-n <- 4
-partGranger(EuStockMarkets[,1:n],nx=1,ny=1,order=3)
-partGranger(EuStockMarkets[,1:n],nx=1,ny=3,order=3)
+ts <- EuStockMarkets
+n <- ncol(ts)
+partGranger(ts[,1:n],nx=1,ny=1,order=3)
+partGranger(ts[,1:n],nx=1,ny=3,order=3)
 for(i in 1:(n*(n-1))){
   perm <- permutations(n,2,1:n)[i,]
   perm <- c(perm,setdiff(1:n,perm))
-  prob <- partGranger(EuStockMarkets[,perm],nx=1,ny=1,order=3)$prob
+  prob <- partGranger(ts[,perm],nx=1,ny=1,order=3)$prob
   if(prob <= 0.05){
     print(perm)
     print(prob)
