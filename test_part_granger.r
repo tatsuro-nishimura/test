bbdb155141022de221afa04fac48c86fd7181943
tatsuro-nishimura <ntatsuro127@gmail.com>
2019-03-27 14:32:14 +0900

    change

--- test_part_granger.r
+++ test_part_granger.r
@@ -6,12 +6,13 @@ n <- ncol(ts)
 partGranger(ts[,1:n],nx=1,ny=1,order=3)
 partGranger(ts[,1:n],nx=1,ny=3,order=3)
 alpha <- 0.05
+cn <- colnames(ts)
 for(i in 1:(n*(n-1))){
-  perm <- permutations(n,2,1:n)[i,]
-  perm <- c(perm,setdiff(1:n,perm))
+  perm0 <- permutations(n,2,1:n)[i,]
+  perm <- c(perm0,setdiff(1:n,perm0))
   prob <- partGranger(ts[,perm],nx=1,ny=1,order=3)$prob
   if(prob <= alpha){
-    print(perm)
+    print(paste(cn[perm0[1]],'->',cn[perm0[2]]))
     print(prob)
   }
 }
