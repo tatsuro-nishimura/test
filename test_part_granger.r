ce555fc8be168000272488dc42ee76560312be6a
tatsuro-nishimura <ntatsuro127@gmail.com>
2019-03-27 11:29:30 +0900

    change

--- test_part_granger.r
+++ test_part_granger.r
@@ -5,11 +5,12 @@ ts <- EuStockMarkets
 n <- ncol(ts)
 partGranger(ts[,1:n],nx=1,ny=1,order=3)
 partGranger(ts[,1:n],nx=1,ny=3,order=3)
+alpha <- 0.05
 for(i in 1:(n*(n-1))){
   perm <- permutations(n,2,1:n)[i,]
   perm <- c(perm,setdiff(1:n,perm))
   prob <- partGranger(ts[,perm],nx=1,ny=1,order=3)$prob
-  if(prob <= 0.05){
+  if(prob <= alpha){
     print(perm)
     print(prob)
   }
