36cd21c405c3fc320f0a49da15cb4151ee446aae
tatsuro-nishimura <ntatsuro127@gmail.com>
2019-03-27 15:52:39 +0900

    change

--- test_part_granger.r
+++ test_part_granger.r
@@ -1,5 +1,6 @@
 library(devtools)
 library(gtools)
+library(DiagrammeR)
 source_url('https://raw.githubusercontent.com/cran/FIAR/master/R/partGranger.R')
 ts <- EuStockMarkets
 n <- ncol(ts)
@@ -7,12 +8,17 @@ partGranger(ts[,1:n],nx=1,ny=1,order=3)
 partGranger(ts[,1:n],nx=1,ny=3,order=3)
 alpha <- 0.05
 cn <- colnames(ts)
+dotstr <- 'digraph causality {'
 for(i in 1:(n*(n-1))){
   perm0 <- permutations(n,2,1:n)[i,]
   perm <- c(perm0,setdiff(1:n,perm0))
   prob <- partGranger(ts[,perm],nx=1,ny=1,order=3)$prob
   if(prob <= alpha){
-    print(paste(cn[perm0[1]],'->',cn[perm0[2]]))
+    arrow <- paste(cn[perm0[1]],'->',cn[perm0[2]])
+    print(arrow)
     print(prob)
+    dotstr <- paste0(dotstr,arrow,'[label="',round(prob,5),'@@"];')
   }
 }
+dotstr <- paste0(dotstr, '}')
+grViz(dotstr)
